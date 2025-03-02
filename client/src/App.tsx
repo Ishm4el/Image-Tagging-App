0dd192bb0fd795cb52b74b1cfe90b1e07054422b
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-03-01 23:37:16 -0800

    Implemented the NavigationBar component for the website

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
-import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import appLogo from "/personal-svgrepo-com.svg";
 
 function App() {
   return (
     <>
+      <NavigationBar
+        links={[
+          { linkTitle: "TEST_TITLE1", linkAddress: "/TEST_ADDRESS1" },
+          { linkTitle: "TEST_TITLE2", linkAddress: "/TEST_ADDRESS2" },
+        ]}
+        webTitle={{
+          title: "TEST_WEB_TITLE",
+          svgLink: appLogo,
+        }}
+      />
       <h1>Hello World</h1>
     </>
   );
--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,4 +1,58 @@
 import { Link } from "react-router-dom";
 import styles from "./NavigationBar.module.css";
+import { ReactElement } from "react";
 
+type FormatLinkObject = {
+  linkTitle: string;
+  linkAddress: string;
+};
 
+type FormatTitleObject = {
+  title: string;
+  svgLink: string;
+};
+
+function WebsiteTitle({ title, svgLink }: FormatTitleObject) {
+  return (
+    <div className={styles["card-web-title"]}>
+      <img src={svgLink} alt="" />
+      <span>{title}</span>
+    </div>
+  );
+}
+
+type ListOfLinksProps = {
+  links: Array<FormatLinkObject>;
+};
+function ListOfLinks({ links }: ListOfLinksProps): ReactElement {
+  return (
+    <ul className={styles["container-links"]}>
+      {links.map((link: FormatLinkObject) => {
+        return (
+          <li className={styles["card-link"]} key={link.linkTitle}>
+            <Link className={styles["a-link"]} to={link.linkAddress}>{link.linkTitle}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+type NavigationBarProps = {
+  links: Array<FormatLinkObject>;
+  webTitle: FormatTitleObject;
+};
+function NavigationBar({ links, webTitle }: NavigationBarProps): ReactElement {
+  console.log(webTitle.svgLink);
+
+  return (
+    <div className={styles["container-nav"]}>
+      <nav className={styles["card-nav"]}>
+        <WebsiteTitle title={webTitle.title} svgLink={webTitle.svgLink} />
+        <ListOfLinks links={links} />
+      </nav>
+    </div>
+  );
+}
+
+export default NavigationBar;
