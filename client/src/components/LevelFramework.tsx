e639b4f281c35f00e411c335e1ca54ee72773d80
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-03-06 14:54:55 -0800

    removed the content for the most part from the Level_1.tsx element; going to implement the general logic for the game in an external component called LevelFramework.tsx

---
+++ client/src/components/LevelFramework.tsx
@@ -0,0 +1,8 @@
+import styles from "./LevelFramework.module.css"
+export default function LevelFramework(): React.ReactElement {
+    return(
+        <main classname={styles["container-main"]}>
+            Hello!
+        </main>
+    )
+}
--- client/src/routes/levels/Level_1.tsx
+++ client/src/routes/levels/Level_1.tsx
@@ -1,8 +1,7 @@
+import LevelFramework from "../../components/LevelFramework";
 import styles from "./Level.module.css";
 export default function Level_1(): React.ReactElement {
   return (
-    <main className={styles["container-main"]}>
-      <div className={styles["dummy"]}>RAWR!</div>
-    </main>
+    <LevelFramework />
   );
 }
