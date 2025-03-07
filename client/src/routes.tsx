ae3bdad736d535788263d2738ad20a99b44b4a4b
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-03-06 17:59:30 -0800

    Modularized Stages.tsx. Now the stages can be controlled from the route.tsx. Added types for the Level Card, setting the basis for the future implelmentation of the class selection.

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -5,6 +5,52 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Stages from "./routes/Stages";
 import Level_1 from "./routes/levels/Level_1";
+type Highscore = `${string & { __brand: "\\d{2}:\\d{2}:\\d{2}" }}`;
+type CardLevelDescriptionProps = {
+  title: string;
+  difficulty: "Easy" | "Medium" | "Hard";
+  href: string;
+  thumbnailLink: string;
+  highscore: Highscore;
+};
+const stagesLevelsArray: Array<CardLevelDescriptionProps> = [
+  {
+    difficulty: "Easy",
+    href: "Level_1",
+    title: "Level 1",
+    thumbnailLink: "A_Thumbnail_Link",
+    highscore: "00:00:00" as Highscore,
+  },
+  {
+    difficulty: "Easy" as "Easy",
+    href: "Level_1",
+    title: "Level 1",
+    thumbnailLink: "A_Thumbnail_Link",
+    highscore: "00:00:00" as Highscore,
+  },
+  {
+    difficulty: "Easy" as "Easy",
+    href: "Level_1",
+    title: "Level 1",
+    thumbnailLink: "A_Thumbnail_Link",
+    highscore: "00:00:00" as Highscore,
+  },
+  {
+    difficulty: "Easy" as "Easy",
+    href: "Level_1",
+    title: "Level 1",
+    thumbnailLink: "A_Thumbnail_Link",
+    highscore: "00:00:00" as Highscore,
+  },
+  {
+    difficulty: "Easy" as "Easy",
+    href: "Level_1",
+    title: "Level 1",
+    thumbnailLink: "A_Thumbnail_Link",
+    highscore: "00:00:00" as Highscore,
+  },
+];
+
 const routes: RouteObject[] = [
   {
     path: "/",
@@ -16,7 +62,10 @@ const routes: RouteObject[] = [
       {
         path: "Stages",
         children: [
-          { index: true, element: <Stages /> },
+          {
+            index: true,
+            element: <Stages levels={stagesLevelsArray} />,
+          },
           { path: "Level_1", element: <Level_1 /> },
         ],
       },
--- client/src/routes/Stages.tsx
+++ client/src/routes/Stages.tsx
@@ -1,71 +1,68 @@
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import styles from "./Stages.module.css";
 
-export default function Stages(): React.ReactElement {
+type CardLevelDescriptionProps = {
+  title: string;
+  difficulty: "Easy" | "Medium" | "Hard";
+  href: string;
+  thumbnailLink: string;
+  highscore: string;
+};
+function CardLevelDescription({
+  title,
+  difficulty,
+  href,
+  thumbnailLink,
+  highscore,
+}: CardLevelDescriptionProps): React.ReactElement {
   const navigate: NavigateFunction = useNavigate();
+
+  return (
+    <div className={styles["card-stage"]}>
+      <div className={styles["card-stage-left"]}>
+        <dl>
+          <dt>Title</dt>
+          <dd>{title}</dd>
+          <dt>Difficulty</dt>
+          <dd>{difficulty}</dd>
+          <dt>Highscore</dt>
+          <dd>{highscore}</dd>
+        </dl>
+      </div>
+      <div className={styles["card-stage  -right"]}>
+        <img src={thumbnailLink} alt={thumbnailLink} />
+        <button
+          onClick={() => {
+            navigate(href);
+          }}
+        >
+          Play
+        </button>
+      </div>
+    </div>
+  );
+}
+
+type StagesProps = {
+  levels: Array<CardLevelDescriptionProps>;
+};
+export default function Stages({ levels }: StagesProps): React.ReactElement {
+  const levelCards = levels.map((level) => (
+    <CardLevelDescription
+      difficulty={level.difficulty}
+      href={level.href}
+      title={level.title}
+      highscore={level.highscore}
+      thumbnailLink={level.thumbnailLink}
+    />
+  ));
 
   return (
     <main className={styles["container-main"]}>
       <header>
         <h1>STAGES</h1>
       </header>
-      <section className={styles["dashboard-stages"]}>
-        <div className={styles["card-stage"]}>
-          <dl>
-            <dt>Title</dt>
-            <dd>Level 1</dd>
-            <dt>Difficulty</dt>
-            <dd>Easy</dd>
-          </dl>
-          <button
-            onClick={() => {
-              navigate("Level_1");
-            }}
-          >
-            Play
-          </button>
-        </div>
-        <div className={styles["card-stage"]}>
-          <dl>
-            <dt>Title</dt>
-            <dd>Level 1</dd>
-            <dt>Difficulty</dt>
-            <dd>Easy</dd>
-          </dl>
-        </div>
-        <div className={styles["card-stage"]}>
-          <dl>
-            <dt>Title</dt>
-            <dd>Level 1</dd>
-            <dt>Difficulty</dt>
-            <dd>Easy</dd>
-          </dl>
-        </div>
-        <div className={styles["card-stage"]}>
-          <dl>
-            <dt>Title</dt>
-            <dd>Level 1</dd>
-            <dt>Difficulty</dt>
-            <dd>Easy</dd>
-          </dl>
-        </div>
-        <div className={styles["card-stage"]}>
-          <dl>
-            <dt>Title</dt>
-            <dd>Level 1</dd>
-            <dt>Difficulty</dt>
-            <dd>Easy</dd>
-          </dl>
-        </div>
-        <div className={styles["card-stage"]}>
-          <dl>
-            <dt>Title</dt>
-            <dd>Level 1</dd>
-            <dt>Difficulty</dt>
-            <dd>Easy</dd>
-          </dl>
-        </div>
-      </section>
+      <section className={styles["dashboard-stages"]}>{levelCards}</section>
     </main>
   );
 }
