5b60f7b7288807cf9ed9fcebfff262106ae2b373
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-03-11 04:48:05 -0700

    Implemented most of the interactivity that the levelframework is going to be functioning on. The letter to be selected is now present and coordinates according to the image are now calculated based on the click. added a new key-value pair to stagesLevelsArray - now includes an array of objects that contains the letters to be founded on a level.

--- client/src/components/LevelFramework.tsx
+++ client/src/components/LevelFramework.tsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import styles from "./LevelFramework.module.css";
 import imageSources from "../imageSourceConfig";
+import { useState } from "react";
+import levelsInfo from "../levelsConfig";
+
 export default function LevelFramework(): React.ReactElement {
   const { stage } = useParams();
   if (stage !== undefined) {
@@ -8,17 +11,67 @@ export default function LevelFramework(): React.ReactElement {
     const stageNumber: string = arraySplitStage[1];
     const imageLink: string =
       "/level_images/large_level_" + stageNumber + ".jpg";
+    const thisLevelInfo = levelsInfo[Number(stageNumber) - 1];
+    const [letters, setLetters] = useState(
+      thisLevelInfo.letters.map((letter, index) => {
+        if (index !== 0)
+          return {
+            active: false,
+            letter: letter.letter,
+          };
+        else return { active: true, letter: letter.letter };
+      })
+    );
     return (
       <main className={styles["container-main"]}>
-        <img
-          className={styles["image-level"]}
-          src={imageLink}
-          alt={
-            "wooden blocks with letters; source: " + imageSources[stageNumber]
-          }
-        />
-        <p>{stage}</p>
-        <p>Hello!</p>
+        <h1>{arraySplitStage[0] + " " + arraySplitStage[1]}</h1>
+        <div className={styles["image-container"]}>
+          <img
+            className={styles["image-level"]}
+            src={imageLink}
+            alt={
+              "wooden blocks with letters; source: " + imageSources[stageNumber]
+            }
+            onClick={(ev) => {
+              const target = ev.target as HTMLImageElement;
+              const rect = target.getBoundingClientRect();
+              const x = ev.clientX - rect.left;
+              const y = ev.clientY - rect.top;
+              const xPercent = x / rect.width;
+              const yPercent = y / rect.height;
+              const actualX = xPercent * target.naturalWidth;
+              const actualY = yPercent * target.naturalHeight;
+              console.log("left? : ", actualX, "; top: ", actualY);
+            }}
+          />
+        </div>
+        <div>
+          {letters.map((letter) => (
+            <button
+              onClick={(ev) => {
+                const theLetter: string = (ev.target as HTMLElement).innerHTML;
+                setLetters((prevLetters) => {
+                  const newLetters = prevLetters.map((letter) => {
+                    if (letter.letter !== theLetter) letter.active = false;
+                    else letter.active = true;
+                    return letter;
+                  });
+                  return newLetters;
+                });
+              }}
+              key={letter.letter}
+              className={
+                styles[
+                  letter.active
+                    ? "letter-button-active"
+                    : "letter-button-inactive"
+                ]
+              }
+            >
+              {letter.letter}
+            </button>
+          ))}
+        </div>
       </main>
     );
   }
