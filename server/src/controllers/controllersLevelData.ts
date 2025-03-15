934b88794efd42a07b48dbe7550c41888b580a93
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-03-14 19:45:16 -0700

    omitted leveltitle from the get

--- server/src/controllers/controllersLevelData.ts
+++ server/src/controllers/controllersLevelData.ts
@@ -18,6 +18,7 @@ const getBasicStage = async (req: Request, res: Response): Promise<any> => {
   const levelName = req.params.stage.replace("_", " ");
   const row = await prisma.letter.findMany({
     where: { leveltitle: levelName },
+    omit: { leveltitle: true },
   });
   return res.json(row);
 };
