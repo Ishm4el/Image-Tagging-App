18393183a9039fa74a6b90b14c0d59117a43dcc7
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-04-11 18:47:50 -0700

    Integrated asyncHandler to handle errors in the controllers

--- server/src/controllers/controllersLevelData.ts
+++ server/src/controllers/controllersLevelData.ts
@@ -1,41 +1,47 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import prisma from "../../prisma/prisma";
+import asyncHandler from "express-async-handler";
 
-const getStages = async (req: Request, res: Response): Promise<any> => {
-  const rows = await prisma.levels.findMany({
-    orderBy: { title: "asc" },
-    include: { scoreboard: { orderBy: { score: "desc" }, take: 1 } },
-  });
-  return res.json(rows);
-};
+const getStages: RequestHandler = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const rows = await prisma.levels.findMany({
+      orderBy: { title: "asc" },
+      include: { scoreboard: { orderBy: { score: "desc" }, take: 1 } },
+    });
+    return res.json(rows);
+  }
+);
 
-const getStagesOnly = async (req: Request, res: Response): Promise<any> => {
-  const rows = await prisma.levels.findMany({ orderBy: { title: "asc" } });
-  return res.json(rows);
-};
+const getStagesOnly: RequestHandler = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const rows = await prisma.levels.findMany({ orderBy: { title: "asc" } });
+    return res.json(rows);
+  }
+);
 
-const getBasicStage = async (req: Request, res: Response): Promise<any> => {
-  const levelName = req.params.stage.replace("_", " ");
-  const row = await prisma.letter.findMany({
-    where: { leveltitle: levelName },
-  });
-  return res.json(row);
-};
+const getBasicStage: RequestHandler = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const levelName = req.params.stage.replace("_", " ");
+    const row = await prisma.letter.findMany({
+      where: { leveltitle: levelName },
+    });
+    return res.json(row);
+  }
+);
 
-const getStageScoreboard = async (
-  req: Request,
-  res: Response
-): Promise<any> => {
-  const levelTitle: string = req.params.stage.replace("_", " ");
-  const levelData = await prisma.levels.findUnique({
-    where: { title: levelTitle },
-    include: {
-      scoreboard: { orderBy: [{ score: "asc" }, { createdAt: "desc" }] },
-    },
-  });
-  console.log("in getStageScoreboard\nPrinting levelData: ", levelData);
+const getStageScoreboard: RequestHandler = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const levelTitle: string = req.params.stage.replace("_", " ");
+    const levelData = await prisma.levels.findUnique({
+      where: { title: levelTitle },
+      include: {
+        scoreboard: { orderBy: [{ score: "asc" }, { createdAt: "desc" }] },
+      },
+    });
+    console.log("in getStageScoreboard\nPrinting levelData: ", levelData);
 
-  res.json(levelData);
-};
+    res.json(levelData);
+  }
+);
 
 export { getStages, getStagesOnly, getBasicStage, getStageScoreboard };
--- server/src/controllers/controllersTaggingGame.ts
+++ server/src/controllers/controllersTaggingGame.ts
@@ -1,43 +1,46 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
+import asyncHandler from "express-async-handler";
 import prisma from "../../prisma/prisma";
 import { decodeToken, signToken } from "../utility/jwtHandler";
 import { JwtPayload } from "jsonwebtoken";
 
-const confirmCoord = async (req: Request, res: Response) => {
-  const userInput: {
-    x: number;
-    y: number;
-    letter: string;
-    levelTitle: string;
-  } = req.body;
-  console.log(userInput);
-  const row = await prisma.letter.findUnique({
-    where: {
-      leveltitle_letter: {
-        letter: userInput.letter,
-        leveltitle: userInput.levelTitle,
+const confirmCoord: RequestHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userInput: {
+      x: number;
+      y: number;
+      letter: string;
+      levelTitle: string;
+    } = req.body;
+    console.log(userInput);
+    const row = await prisma.letter.findUnique({
+      where: {
+        leveltitle_letter: {
+          letter: userInput.letter,
+          leveltitle: userInput.levelTitle,
+        },
       },
-    },
-    select: { length: true, x: true, y: true },
-  });
-  console.log(row);
-
-  if (
-    row &&
-    userInput.x >= row.x &&
-    userInput.x <= row.x + row.length &&
-    userInput.y >= row.y &&
-    userInput.y <= row.y + row.length
-  ) {
-    // console.log("within box");
-    const token = signToken({
-      letter: userInput.letter,
-      found: true,
-      levelTitle: userInput.levelTitle,
+      select: { length: true, x: true, y: true },
     });
-    res.json({ found: true, token });
-  } else res.json({ found: false });
-};
+    console.log(row);
+
+    if (
+      row &&
+      userInput.x >= row.x &&
+      userInput.x <= row.x + row.length &&
+      userInput.y >= row.y &&
+      userInput.y <= row.y + row.length
+    ) {
+      // console.log("within box");
+      const token = signToken({
+        letter: userInput.letter,
+        found: true,
+        levelTitle: userInput.levelTitle,
+      });
+      res.json({ found: true, token });
+    } else res.json({ found: false });
+  }
+);
 
 interface verifyVictory extends JwtPayload {
   letter: string;
@@ -60,54 +63,57 @@ const isLevelToken = (
   return (token as JwtPayload).iat !== undefined;
 };
 
-const confirmVictory = async (req: Request, res: Response) => {
-  const currentTime = Math.floor(Date.now() / 1000);
-  const tokens: Array<string> = req.body.tokens;
-  const levelToken: string = req.body.levelToken;
-  const decodedLevelToken: string | JwtPayload = decodeToken(levelToken);
-  console.log("decodedLevelToken: ", decodedLevelToken);
+const confirmVictory: RequestHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const currentTime = Math.floor(Date.now() / 1000);
+    const tokens: Array<string> = req.body.tokens;
+    const levelToken: string = req.body.levelToken;
+    const decodedLevelToken: string | JwtPayload = decodeToken(levelToken);
+    console.log("decodedLevelToken: ", decodedLevelToken);
 
-  let victory = true;
-  tokens.forEach((token) => {
-    const decodedToken = decodeToken(token);
-    const payload: verifyVictory | undefined = isPayload(decodedToken)
-      ? decodedToken
-      : undefined;
-    if (
-      payload &&
-      (payload.found === false ||
-        (typeof decodedLevelToken === "object" &&
-          decodedLevelToken.levelTitle !== payload.levelTitle))
-    ) {
-      victory = false;
-    }
-  });
+    let victory = true;
+    tokens.forEach((token) => {
+      const decodedToken = decodeToken(token);
+      const payload: verifyVictory | undefined = isPayload(decodedToken)
+        ? decodedToken
+        : undefined;
+      if (
+        payload &&
+        (payload.found === false ||
+          (typeof decodedLevelToken === "object" &&
+            decodedLevelToken.levelTitle !== payload.levelTitle))
+      ) {
+        victory = false;
+      }
+    });
 
-  const intervalCalc =
-    currentTime - (isLevelToken(decodedLevelToken) ? decodedLevelToken.iat : 0);
+    const intervalCalc =
+      currentTime -
+      (isLevelToken(decodedLevelToken) ? decodedLevelToken.iat : 0);
 
-  const format = new Date(intervalCalc * 1000);
-  const minutes = "0" + format.getMinutes();
-  const seconds = "0" + format.getSeconds();
-  const toRecord =
-    minutes.substring(seconds.length - 2) +
-    ":" +
-    seconds.substring(seconds.length - 2);
-  const finalToken = signToken({
-    endTime: new Date(currentTime * 1000),
-    startTime: new Date(
-      (isLevelToken(decodedLevelToken) ? decodedLevelToken.iat : 0) * 1000
-    ),
-    score: toRecord,
-    leveltitle: isLevelToken(decodedLevelToken)
-      ? decodedLevelToken.levelTitle
-      : "ERROR",
-  });
+    const format = new Date(intervalCalc * 1000);
+    const minutes = "0" + format.getMinutes();
+    const seconds = "0" + format.getSeconds();
+    const toRecord =
+      minutes.substring(seconds.length - 2) +
+      ":" +
+      seconds.substring(seconds.length - 2);
+    const finalToken = signToken({
+      endTime: new Date(currentTime * 1000),
+      startTime: new Date(
+        (isLevelToken(decodedLevelToken) ? decodedLevelToken.iat : 0) * 1000
+      ),
+      score: toRecord,
+      leveltitle: isLevelToken(decodedLevelToken)
+        ? decodedLevelToken.levelTitle
+        : "ERROR",
+    });
 
-  res.json({ victory, finalToken, yourScore: toRecord });
-};
+    res.json({ victory, finalToken, yourScore: toRecord });
+  }
+);
 
-const start = (req: Request, res: Response) => {
+const start: RequestHandler = (req: Request, res: Response) => {
   const token = signToken({ levelTitle: req.body.levelTitle });
   res.json(token);
 };
@@ -117,40 +123,42 @@ interface finalToken extends verifyLevelToken {
   score: string;
 }
 
-const complete = async (req: Request, res: Response) => {
-  const finalToken = decodeToken(req.body.finalToken) as finalToken;
-  const userName = req.body.userName;
+const complete: RequestHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const finalToken = decodeToken(req.body.finalToken) as finalToken;
+    const userName = req.body.userName;
 
-  const row = await prisma.score.create({
-    data: {
-      endTime: finalToken.endTime,
-      startTime: new Date(finalToken.iat * 1000),
-      score: finalToken.score,
-      userName: userName,
-      leveltitle: finalToken.leveltitle,
-    },
-  });
+    const row = await prisma.score.create({
+      data: {
+        endTime: finalToken.endTime,
+        startTime: new Date(finalToken.iat * 1000),
+        score: finalToken.score,
+        userName: userName,
+        leveltitle: finalToken.leveltitle,
+      },
+    });
 
-  const title: string = finalToken.leveltitle;
-  console.log("title: ", title);
+    const title: string = finalToken.leveltitle;
+    console.log("title: ", title);
 
-  // determine user's ranking
-  const leaderBoardPlacement = await prisma.levels.findUnique({
-    where: { title: title },
-    include: {
-      scoreboard: { orderBy: [{ score: "asc" }, { createdAt: "desc" }] },
-    },
-  });
+    // determine user's ranking
+    const leaderBoardPlacement = await prisma.levels.findUnique({
+      where: { title: title },
+      include: {
+        scoreboard: { orderBy: [{ score: "asc" }, { createdAt: "desc" }] },
+      },
+    });
 
-  console.log("row.id: ", row.id);
-  const scoreboard = leaderBoardPlacement?.scoreboard;
+    console.log("row.id: ", row.id);
+    const scoreboard = leaderBoardPlacement?.scoreboard;
 
-  const indexOfUserScore = scoreboard?.findIndex((e) => e.id === row.id)! + 1;
+    const indexOfUserScore = scoreboard?.findIndex((e) => e.id === row.id)! + 1;
 
-  // console.log("indexOfUserScore: ", indexOfUserScore);
-  console.log("leaderBoardPlacement: ", leaderBoardPlacement);
+    // console.log("indexOfUserScore: ", indexOfUserScore);
+    console.log("leaderBoardPlacement: ", leaderBoardPlacement);
 
-  res.json({ done: true, row, indexOfUserScore });
-};
+    res.json({ done: true, row, indexOfUserScore });
+  }
+);
 
 export { confirmCoord, confirmVictory, start, complete };
